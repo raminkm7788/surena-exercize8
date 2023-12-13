471c8886870c04807be4b5834a2fb0b67ae66488
Sepand <Sepand@SEPAND>
2023-12-13 23:38:17 +0330

    Add project files.

---
+++ surena exercize8/Cost.cs
@@ -0,0 +1,14 @@
+﻿public class Cost
+{
+    
+    public string vahed { get; set; }
+
+    public double gheymat { get; set; }
+    public double meghdar_rozane { get; set; }
+    public override string ToString()
+    {
+        return $"vahed={vahed} gheymat={gheymat} meghdar _rozane={meghdar_rozane}";
+    }
+
+
+}
---
+++ surena exercize8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+List<Animal> animals = new List<Animal>()
+{
+new COW() { name = "ALIS", Ismale = true, weight = 400, Birthday= new DateTime(2022,10,12) },
+};
+
+
+
+foreach (Animal animal in animals)
+{
+    Console.WriteLine(animal);
+
+}
